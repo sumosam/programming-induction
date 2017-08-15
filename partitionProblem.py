dc6df4a988c172d678f52fd47c6eb238a89a6662
Saumitra <saumitra.saha@gmail.com>
2017-08-15 12:41:24 -0700

    boiler plate code

---
+++ partitionProblem.py
@@ -0,0 +1,23 @@
+import unittest
+#The partition problem
+
+#Can a given set of numbers be partitioned into 
+#two subsets such that the sum of elements in both subsets
+#are the same. 
+
+#Examples given:
+# arr = [1, 5, 11, 5]
+# output: true
+
+# arr = [1, 5, 3]
+# output: false
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  unittest.main()
